6f77709813bc273a3f7b27c8edaddbee4828cd5e
Osmar <osmar.gascho@nbwdigital.com.br>
2022-06-07 15:33:02 -0300

    fix

--- shell/import_employees.py
+++ shell/import_employees.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     main_oc_conn=oc_conn()
 
     employees=oc_select("SELECT * FROM R034FUN",main_oc_conn)
-    sql_select("TRUNCATE lunellicarreiras.employees",main_sql_conn)
-    for emp in employees:
-        insert_carreiras="INSERT INTO lunellicarreiras.employees (name,senior_num_cad,senior_num_emp,senior_cod_fil,senior_tip_col) VALUES('"+str(emp["NOMFUN"])+"','"+str(emp["NUMCAD"])+"','"+str(emp["NUMEMP"])+"','"+str(emp["CODFIL"])+"','"+str(emp["TIPCOL"])+"')"
-        run_sql(insert_carreiras,main_sql_conn)
+    if len(employees)>0:
+        sql_select("TRUNCATE lunellicarreiras.employees",main_sql_conn)
+        for emp in employees:
+            insert_carreiras="INSERT INTO lunellicarreiras.employees (name,senior_num_cad,senior_num_emp,senior_cod_fil,senior_tip_col) VALUES('"+str(emp["NOMFUN"])+"','"+str(emp["NUMCAD"])+"','"+str(emp["NUMEMP"])+"','"+str(emp["CODFIL"])+"','"+str(emp["TIPCOL"])+"')"
+            run_sql(insert_carreiras,main_sql_conn)
