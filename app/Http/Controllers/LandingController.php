8d499028e76887e3e911a4e1fde247b16bf82beb
Osmar Gascho <osmar.malks@gmail.com>
2023-05-08 17:46:36 -0300

    Update LandingController.php

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -186,7 +186,9 @@ class LandingController extends Controller
     }
 
     public function sendHelp(Request $request){
+        echo "fechado";
         return false;
+        echo "hmm";
         $logged_in=Auth::user();
         $data=$request->all();
         if ($request->hasFile('contact_file')){
