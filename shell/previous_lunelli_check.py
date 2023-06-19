d41c66250bd6d5234fb1e3535564d3618b170df2
Osmar <osmar.gascho@nbwdigital.com.br>
2023-06-19 08:02:37 -0300

    fix

--- shell/previous_lunelli_check.py
+++ shell/previous_lunelli_check.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     candidates=sql_select("SELECT ca.id,c.nome,c.cpf,c.nomeFilial,c.cadastro,c.cargo,c.createdAt,ca.name,ca.cpf,ca.senior_num_can,ca.previous_lunelli_cad,ca.previous_lunelli_unit,ca.previous_lunelli_job,ca.previous_lunelli_date  FROM b2blunender.Colaborador c JOIN lunellicarreiras.candidates ca ON ca.cpf=c.cpf WHERE ca.previous_lunelli_cad IS NULL AND ca.senior_num_can IS NULL AND ca.cpf IS NOT NULL AND ca.cpf!=''",main_sql_conn)
     
     for cnd in candidates:
-        sql="UPDATE candidates SET previous_lunelli_cad='"+str(cnd["cadastro"])+"',previous_lunelli_unit='"+str(cnd["nomeFilial"])+"',previous_lunelli_job='"+str(cnd["cargo"])+"',previous_lunelli_date='"+str(cnd["createdAt"])+"',updated_at=updated_at WHERE id='"+str(cnd["id"])+"'"
+        sql="UPDATE lunellicarreiras.candidates SET previous_lunelli_cad='"+str(cnd["cadastro"])+"',previous_lunelli_unit='"+str(cnd["nomeFilial"])+"',previous_lunelli_job='"+str(cnd["cargo"])+"',previous_lunelli_date='"+str(cnd["createdAt"])+"',updated_at=updated_at WHERE id='"+str(cnd["id"])+"'"
         run_sql(sql,main_sql_conn)
-        addtagsql="INSERT IGNORE INTO candidates_tagsrh (candidate_id,tag_id,created_at,_updated_at) VALUES('"+str(cnd["id"])+"',5,now(),now())"
+        addtagsql="INSERT IGNORE INTO lunellicarreiras.candidates_tagsrh (candidate_id,tag_id,created_at,_updated_at) VALUES('"+str(cnd["id"])+"',5,now(),now())"
         run_sql(addtagsql,main_sql_conn)
 
     #Fecha as conexões principais
